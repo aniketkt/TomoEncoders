277d032546cf97c6f951f85cbbe43b5b8bb75d4f
Aniket Tekawade <aniketkt@gmail.com>
2021-10-18 16:57:22 -0500

    ADD: sparse segmenter task

---
+++ tomo_encoders/tasks/sparse_segmenter/recon_subvol.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+
+
+"""
+import numpy as np
+import cupy as cp
+import time
+import h5py
+from cupyx.scipy.fft import rfft, irfft, rfftfreq
+
+def darkflat_correction(data, dark, flat):
+    """Dark-flat field correction"""
+    for k in range(data.shape[0]):
+        data[k] = (data[k]-dark)/cp.maximum(flat-dark, 1e-6)
+    return data
+def minus_log(data):
+    """Taking negative logarithm"""
+    data = -cp.log(cp.maximum(data, 1e-6))
+    return data
+
+def fbp_filter(data):
+    """FBP filtering of projections"""
+    t = rfftfreq(data.shape[2])
+    wfilter = t #* (1 - t * 2)**3  # parzen
+    wfilter = cp.tile(wfilter, [data.shape[1], 1])
+    # loop over slices to minimize fft memory overhead
+    for k in range(data.shape[0]):
+        data[k] = irfft(
+            wfilter*rfft(data[k], overwrite_x=True, axis=1), overwrite_x=True, axis=1)
+    return data
+
+source = """
+extern "C" {
+    void __global__ rec(float *f, float *g, float *theta, float center, int ntheta, int nz, int n, int stx, int px, int sty, int py, int stz, int pz)
+    {
+        int tx = blockDim.x * blockIdx.x + threadIdx.x;
+        int ty = blockDim.y * blockIdx.y + threadIdx.y;
+        int tz = blockDim.z * blockIdx.z + threadIdx.z;
+        if (tx >= px || ty >= py || tz>=pz)
+            return;
+        stx += tx;
+        sty += ty;
+        stz += tz;
+        int s0 = 0;
+        int ind = 0;
+        float f0 = 0;
+        float sp = 0;
+        
+        for (int k = 0; k < ntheta; k++)
+        {
+            sp = (stx - n / 2) * __cosf(theta[k]) - (sty - n / 2) * __sinf(theta[k]) + center; //polar coordinate
+            //linear interpolation
+            s0 = roundf(sp);
+            ind = k * n * nz + stz * n + s0;
+            if ((s0 >= 0) & (s0 < n - 1))
+                f0 += g[ind] + (g[ind+1] - g[ind]) * (sp - s0) / n;
+        }
+        f[tz*px*py+ty*px+tx] = f0;
+    }
+}
+"""
+module = cp.RawModule(code=source)
+rec_kernel = module.get_function('rec')
+def rec(data, theta, center, stx, px, sty, py, stz, pz):
+    """Reconstruct subvolume [stz:stz+pz,sty:sty+py,stx:stx+px] on GPU"""
+    [ntheta, nz, n] = data.shape
+    obj = cp.zeros([pz, py, px], dtype='float32')
+    rec_kernel((int(cp.ceil(px/16)), int(cp.ceil(py/16)), int(cp.ceil(pz/4))), (16, 16, 4),
+                  (obj, data, theta, cp.float32(center), ntheta, nz, n, stx, px, sty, py, stz, pz))
+    return obj
+
+
+
+def recon_patch(projs, theta, center, point, width, mem_limit_gpu = 5.0, apply_fbp = True):
+    
+    '''
+    reconstruct a region within full volume shaped as cuboid, defined corner points (z, y, x) and widths (wz, wy, wx).  
+    
+    Parameters
+    ----------
+    projs : np.ndarray  
+        array of projection images shaped as ntheta, nrows, ncols
+    theta : np.ndarray
+        array of theta values (length = ntheta)  
+    center : float  
+        center value for the projection data  
+    mem_limit_gpu : float  
+        mem limit in GB for GPU  
+    point : np.ndarray  
+        array of 3 corner points z, y, x  
+    width : np.ndarray  
+        array of 3 widths wz, wy, wx  
+    
+
+    
+    Returns
+    -------
+    
+    '''
+    
+    sz = slice(point[0], point[0] + width[0])
+    # first crop the projections along z
+    projs = projs[:, sz, :].copy()
+    
+    # pad values
+    padding = int(projs.shape[-1]*(3/8.0))
+    projs = np.pad(projs, ((0,0),(0,0),(padding, padding)), mode = 'edge')
+    
+    # send to gpu; check if memory limit is crossed  
+    # Q: what should be memory limit?  
+    if projs.nbytes/1.0e9 > mem_limit_gpu:
+        raise ValueError("mem limit breached")
+    else:
+        data = cp.array(projs)
+        theta = cp.array(theta, dtype = 'float32')
+        if apply_fbp:
+            data = fbp_filter(data) # need to apply filter to full projection  
+            data = data #- cp.mean(data, axis = (0,2))
+        center = cp.float32(center)    
+    
+    # st* - start, p* - number of points
+    stz, sty, stx = point
+    pz, py, px = width
+    st = time.time()
+    obj = rec(data, theta, center+padding, \
+              stx+padding, px, \
+              sty+padding, py, \
+              0, pz) # 0 since projections were cropped vertically
+    cp.cuda.stream.get_current_stream().synchronize()
+    print(time.time()-st)
+    
+    return obj.get()
+
+
+if __name__ == "__main__":
+    
+    print('just a bunch of functions')
+    
---
+++ tomo_encoders/tasks/sparse_segmenter/sparse_segmenter.py
@@ -0,0 +1,350 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+class implementations for real-time 3D feature extraction
+
+
+"""
+
+import pandas as pd
+import os
+import glob
+import numpy as np
+
+
+# from skimage.feature import match_template
+# from tomopy import normalize, minus_log, angles, recon, circ_mask
+# from scipy.ndimage.filters import median_filter
+
+from tomo_encoders.neural_nets.porosity_encoders import build_Unet_3D, custom_objects_dict
+from tomo_encoders.patches import Patches
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import UpSampling3D
+from multiprocessing import Pool, cpu_count
+import functools
+
+import h5py
+import abc
+import time
+
+
+# complete once you write one or two feature extractors
+# class AnyFeatureExtractor(metaclass = abc.ABCMeta):
+    
+#     @abc.abstractmethod
+#     def do_something(self):
+#         pass
+#     @abc.abstractmethod
+#     def do_something_else(self):
+#         pass
+
+
+class SparseSegmenter():
+    def __init__(self, vol_shape, \
+                 model_initialization = "define-new", \
+                 model_size = (64,64,64), \
+                 descriptor_tag = "misc", **model_params):
+        '''
+        
+        Parameters
+        ----------
+        vol_shape : tuple
+            Shape of the object volume  
+        
+        model_initialization : str
+            either "define-new" or "load-model"
+        
+        model_size : tuple
+            shape of the input patch required by model
+
+        descriptor_tag : str
+            some description used while saving the models
+
+        model_params : dict
+            If "define-new", this contains the hyperparameters that define the architecture of the neural network model. If "load-model", it contains the paths to the models.
+            
+        models : dict of tf.keras.Models.model 
+            dict contains {"latent_embedder" : encoder_model, "CAE" : denoiser_model}. Here, both models are keras 3D models with input shape = model_size
+
+        '''
+        model_getters = {"load-model" : self._load_models, \
+                         "define-new" : self._build_models, \
+                         "load-weights" : self._load_weights}
+        
+        self.vol_shape = vol_shape
+        # any function chosen must assign self.models, self.model_tag and self.model_size
+        
+        if model_initialization == "define-new":
+            model_getters[model_initialization](model_size = model_size, \
+                                                descriptor_tag = descriptor_tag, \
+                                                **model_params)
+        elif model_initialization == "load-model":
+            model_getters[model_initialization](**model_params)
+        else:
+            raise NotImplementedError("method is not implemented")
+            
+        return
+            
+    def save_models(self, model_path):
+        
+        for model_key in self.models.keys():
+            self.models[model_key].save(os.path.join(model_path, "%s_%s.hdf5"%(model_key, self.model_tag)))
+        return
+    
+    def _build_models(self, model_size = (64,64,64), descriptor_tag = "misc", **model_params):
+        '''
+        
+        Parameters
+        ----------
+        model_keys : list  
+            list of strings describing the model, e.g., ["segmenter"], etc.
+        model_params : dict
+            for passing any number of model hyperparameters necessary to define the model(s).
+            
+        '''
+        if model_params is None:
+            raise ValueError("Need model hyperparameters or instance of model. Neither were provided")
+        else:
+            self.models = {}
+
+        # insert your model building code here. The models variable must be a dictionary of models with str descriptors as keys
+        self.model_size = model_size
+        self.model_tag = "Unet_%s"%(descriptor_tag)
+
+        model_keys = ["segmenter"]
+        for key in model_keys:
+            self.models.update({key : None})
+        self.models["segmenter"] = build_Unet_3D(self.model_size + (1,), **model_params)
+        self.models["segmenter"].compile(optimizer=tf.keras.optimizers.Adam(),\
+                      loss=tf.keras.losses.BinaryCrossentropy())
+        return
+    
+    def _load_weights(self, **kwargs):
+        raise NotImplementedError("Not implemented yet")
+        
+    def print_layers(self, modelkey):
+        
+        txt_out = []
+        for ii in range(len(self.models[modelkey].layers)):
+            lshape = str(self.models[modelkey].layers[ii].output_shape)
+            lname = str(self.models[modelkey].layers[ii].name)
+            txt_out.append(lshape + "    ::    "  + lname)
+            
+        print('\n'.join(txt_out))
+        return
+        
+    def _load_models(self, model_names = None, model_path = 'some/path'):
+        
+        '''
+        Parameters
+        ----------
+        model_names : dict
+            example {"segmenter" : "Unet"}
+        model_path : str  
+            example "some/path"
+        custom_objects_dict : dict  
+            dictionary of custom objects (usually pickled with the keras models)
+            
+        '''
+        self.models = {} # clears any existing models linked to this class!!!!
+        for model_key, model_name in model_names.items():
+            self.models.update({model_key : load_model(os.path.join(model_path, model_name + '.hdf5'), \
+                                                      custom_objects = custom_objects_dict)})
+        # insert assignment of model_size here
+        self.model_size = self.models["segmenter"].input_shape[1:-1]
+        self.model_tag = "_".join(model_names["segmenter"].split("_")[1:])
+        return
+
+    def _normalize_volume(self, vol):
+        '''
+        Normalizes volume to values into range [0,1]  
+
+        '''
+        if vol.shape != self.vol_shape:
+            raise ValueError("vol shape does not match")
+        else:
+            eps = 1e-12
+            max_val = np.max(vol)
+            min_val = np.min(vol)
+            vol = (vol - min_val) / (max_val - min_val + eps)
+            return vol
+        
+    def data_generator(self, X, Y, batch_size, sampling_method, max_stride = 1, random_rotate = False, add_noise = 0.1):
+
+        
+        '''
+        
+        Parameters  
+        ----------  
+        vol : np.array  
+            Volume from which patches are extracted.  
+        batch_size : int  
+            Size of the batch generated at every iteration.  
+        sampling_method : str  
+            Possible methods include "random", "random-fixed-width", "grid"  
+        max_stride : int  
+            If method is "random" or "multiple-grids", then max_stride is required.  
+        
+        '''
+        
+        while True:
+            
+            if sampling_method in ["grid", "random-fixed-width"]:
+                patches = Patches(X.shape, initialize_by = sampling_method, \
+                                  patch_size = self.model_size, \
+                                  stride = max_stride, \
+                                  n_points = batch_size)    
+                
+            elif sampling_method in ["random"]:
+                patches = Patches(X.shape, initialize_by = sampling_method, \
+                                  min_patch_size = self.model_size, \
+                                  max_stride = max_stride, \
+                                  n_points = batch_size)    
+
+            y = patches.extract(Y, self.model_size)[...,np.newaxis]
+            x = patches.extract(X, self.model_size)[...,np.newaxis]
+            std_batch = np.random.uniform(0, add_noise, batch_size)
+            x = y + np.asarray([np.random.normal(0, std_batch[ii], y.shape[1:]) for ii in range(batch_size)])
+
+            if random_rotate:
+                nrots = np.random.randint(0, 4, batch_size)
+                for ii in range(batch_size):
+                    axes = tuple(np.random.choice([0, 1, 2], size=2, replace=False))
+                    x[ii, ..., 0] = np.rot90(x[ii, ..., 0], k=nrots[ii], axes=axes)
+                    y[ii, ..., 0] = np.rot90(y[ii, ..., 0], k=nrots[ii], axes=axes)
+
+            yield x, y           
+        
+    def train(self, X, Y, batch_size, sampling_method, n_epochs,\
+                                 random_rotate = False,\
+                                 add_noise = 0.1,\
+                                 max_stride = 1):
+        
+        '''
+        
+        '''
+        # normalize volume, check if shape is compatible.  
+        X = self._normalize_volume(X)
+        
+        # instantiate data generator for use in training.  
+        dg = self.data_generator(X, Y, batch_size, sampling_method, \
+                                 max_stride = max_stride, \
+                                 random_rotate = random_rotate, \
+                                 add_noise = add_noise)
+        
+        tot_steps = 1000
+        val_split = 0.2
+        steps_per_epoch = int((1-val_split)*tot_steps//batch_size)
+        validation_steps = int(val_split*tot_steps//batch_size)
+
+        t0 = time.time()
+        self.models["segmenter"].fit(x = dg, epochs = n_epochs,\
+                  steps_per_epoch=steps_per_epoch,\
+                  validation_steps=validation_steps, verbose = 1)    
+        t1 = time.time()
+        training_time = (t1 - t0)
+        print("training time = %.2f seconds"%training_time)        
+        
+        return
+        
+    
+    def sparse_segment(self, X, max_stride = 4):
+        '''
+        '''
+        X = self._normalize_volume(X)
+        # first pass at max_stride
+        patches = Patches(X.shape, initialize_by = "grid", \
+                          patch_size = self.model_size, stride = max_stride)
+        
+        
+        # convert to sobel map
+        Y_edge = self._edge_map(Y_coarse)
+        
+        # extract patches to compute if it contains any edge voxels
+        new_patch_size = tuple(np.asarray(self.model_size)//max_stride)
+        p_sel = Patches(new_vol_shape, initialize_by = "grid", patch_size = new_patch_size, stride = 1)
+        sub_vols = p_sel.extract(Y_edge, new_patch_size)        
+        p_sel.add_features(np.std(sub_vols, axis = (1,2,3)) > 0, names = ['has_edges'])
+        sum_ = np.sum(sub_vols, axis = (1,2,3))
+        size_ = np.prod(sub_vols.shape[1:])
+        p_sel.add_features(sum_ == size_, names = ['has_ones'])
+        p_sel.add_features(sum_ == 0, names = ['has_zeros'])
+        
+        # rescale patches to original size
+        p_sel = p_sel.rescale(stride, X.shape)
+        len1 = len(p_sel.points)
+        
+        
+        Y = np.zeros(X.shape, dtype = np.uint8)
+
+        # what to do about those volumes not selected in the big Y array?
+        p_ones = p_sel.filter_by_condition(p_sel.features_to_numpy(['has_ones']))
+        s = p_ones.slices()
+        for idx in range(len(p_ones.points)):
+                Y[s[idx,0], s[idx,1], s[idx,2]] = np.ones(tuple(p_ones.widths[idx,...]), dtype = np.uint8)
+
+        p_zeros = p_sel.filter_by_condition(p_sel.features_to_numpy(['has_zeros']))
+        s = p_zeros.slices()
+        for idx in range(len(p_zeros.points)):
+                Y[s[idx,0], s[idx,1], s[idx,2]] = np.ones(tuple(p_zeros.widths[idx,...]), dtype = np.uint8)
+        
+        
+        # now run predictions on only those patches that were selected
+        p_edges = p_sel.filter_by_condition(p_sel.features_to_numpy(['has_edges']))
+        len0 = len(p_edges.points)
+        Y = self._segment_patches(X, Y, p_edges)
+        
+        t_save = (len0-len1)/len0*100.0
+        print("compute time saving %.2f pc"%t_save)
+        
+        return Y
+    
+    def _edge_map(self, Y):
+        
+        '''
+        this algorithm was inspired by: https://github.com/tomochallenge/tomochallenge_utils/blob/master/foam_phantom_utils.py
+        '''
+        msk = np.zeros_like(Y)
+        tmp = Y[:-1]!=Y[1:]
+        msk[:-1][tmp] = 1
+        msk[1:][tmp] = 1
+        tmp = Y[:,:-1]!=Y[:,1:]
+        msk[:,:-1][tmp] = 1
+        msk[:,1:][tmp] = 1
+        tmp = Y[:,:,:-1]!=Y[:,:,1:]
+        msk[:,:,:-1][tmp] = 1
+        msk[:,:,1:][tmp] = 1
+        return msk > 0
+        
+    def _segment_patches(self, X, Y, patches, upsample = 1, arr_split_infer = 1):
+        
+        t0 = time.time()
+        x = patches.extract(X, self.model_size)
+        x = x[...,np.newaxis]
+        x = np.array_split(x, arr_split_infer)
+        
+        y_pred = []
+        print("\n")
+        for jj in range(len(x)):
+            tmp = self.models["segmenter"].predict(x[jj])
+            tmp = np.round(tmp).astype(np.uint8)
+            if upsample > 1:
+                tmp = UpSampling3D(size=upsample)(tmp)
+            tmp = tmp[...,0]                    
+            y_pred.append(tmp)
+            print("\rDone %2d of %2d"%(jj+1, len(x)), end = "")
+        print("\n")
+        y_pred = np.concatenate(y_pred, axis = 0)
+        
+        s = patches.slices()
+        for idx in range(len(patches.points)):
+                Y[s[idx,0], s[idx,1], s[idx,2]] = y_pred[idx,...]
+        tot_time = time.time() - t0
+        print("Total time for segmentation at stride %i: %.2f seconds"%(upsample, tot_time))
+        return Y
+
+if __name__ == "__main__":
+    
+    print('just a bunch of functions')
+    
