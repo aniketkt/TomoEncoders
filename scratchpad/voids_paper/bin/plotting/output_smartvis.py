FIX: normalization for U-net

scratchpad/voids_paper/bin/plotting/output_smartvis.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     voids_hr = Voids().import_from_disk(voids_hr_path)
     voids_lr = Voids().import_from_disk(voids_lr_path)
     assert len(voids_hr) == len(voids_lr)
-    idxs = np.argsort(voids_hr["sizes"])
+    idxs = np.argsort(voids_hr["sizes"])[::-1]
     voids_hr.select_by_indices(idxs)
     voids_lr.select_by_indices(idxs)    
 

scratchpad/voids_paper/bin/tests/test_smartvis.py
@@ -20,7 +20,7 @@ from tomo_encoders.structures.voids import Voids
 from tomo_encoders import DataFile
 import cupy as cp
 from tomo_encoders.neural_nets.surface_segmenter import SurfaceSegmenter
-
+from tomo_encoders.tasks.surface_determination import coarse_segmentation
 
 
 ######## START GPU SETTINGS ############
@@ -44,8 +44,10 @@ b = 4
 b_K = 4
 sparse_flag = True
 pixel_res = 1.17
-size_um = 5.0
+size_um = -1 # um
 void_rank = 1
+radius_around_void_um = 800.0 # um
+blur_size = 0.5
 # handy code for timing stuff
 # st_chkpt = cp.cuda.Event(); end_chkpt = cp.cuda.Event(); st_chkpt.record()    
 # end_chkpt.record(); end_chkpt.synchronize(); t_chkpt = cp.cuda.get_elapsed_time(st_chkpt,end_chkpt)
@@ -53,7 +55,7 @@ void_rank = 1
 
 ## Output for vis
 ply_lowres = '/home/atekawade/Dropbox/Arg/transfers/runtime_plots/lowres_full.ply'
-ply_highres = '/home/atekawade/Dropbox/Arg/transfers/runtime_plots/highres_around_void.ply'
+ply_highres = '/home/atekawade/Dropbox/Arg/transfers/runtime_plots/highres_around_void_%i.ply'%void_rank
 voids_highres = '/data02/MyArchive/aisteer_3Dencoders/tmp_data/voids_highres'
 voids_lowres = '/data02/MyArchive/aisteer_3Dencoders/tmp_data/voids_lowres'
 
@@ -65,7 +67,6 @@ if __name__ == "__main__":
                          model_path = model_path)    
     fe.test_speeds(128,n_reps = 5, input_size = (wd,wd,wd))    
 
-
     # read data and initialize output arrays
     ## to-do: ensure reconstructed object has dimensions that are a multiple of the (wd,wd,wd) !!    
     hf = h5py.File('/data02/MyArchive/aisteer_3Dencoders/tmp_data/projs_2k.hdf5', 'r')
@@ -75,46 +76,44 @@ if __name__ == "__main__":
     hf.close()
 
     # make sure projection shapes are divisible by the patch width (both binning and full steps)
+    print("BEGIN: Read projection data from disk")
     print(f'\tSTAT: shape of raw projection data: {projs.shape}')
     
     ##### BEGIN ALGORITHM ########
     # guess surface
-    print(f"STEP: visualize all voids with size greater than {size_um:.2f} um")
-    voids_b = Voids().guess_voids(projs, theta, center, b, b_K)    
+    print(f"\nSTEP: visualize all voids with size greater than {size_um:.2f} um")
+    V_bin, rec_min_max = coarse_segmentation(projs, theta, center, b_K, b, blur_size)
+    voids_b = Voids().guess_voids(V_bin, b)    
     voids_b.select_by_size(size_um, pixel_size_um = pixel_res)
-
-    # export all voids into a mesh
-    surf = voids_b.export_void_mesh()
+    voids_b.sort_by_size(reverse = True)
+    surf = voids_b.export_void_mesh_with_texture("sizes")
     surf.write_ply(ply_lowres)
 
     # guess roi around a void
     void_id = np.argsort(voids_b["sizes"])[-void_rank]
-    pix_radius = int(np.cbrt(voids_b["sizes"][void_id])*2)
-    voids_b.select_around_void(void_id, pix_radius)
-    
-    voids_b.write_to_disk(voids_lowres)    
-    
+    voids_b.select_around_void(void_id, radius_around_void_um, pixel_size_um = pixel_res)
+    print(f"\nSTEP: visualize voids in the neighborhood of void id {void_id} at full detail")    
+
     cp.fft.config.clear_plan_cache()
-    print(f"STEP: visualize voids in the neighborhood of void id {void_id} at full detail")
     p_sel, r_fac = voids_b.export_grid(wd)    
-    x_voids, p_voids = process_patches(projs, theta, center, fe, p_sel)
+    x_voids, p_voids = process_patches(projs, theta, center, fe, p_sel, rec_min_max)
     
-    # export voids to vis
+    # export voids
     voids = Voids().import_from_grid(voids_b, x_voids, p_voids)
-    surf = voids.export_void_mesh_with_texture("distance_%i"%void_id)
-    surf.write_ply(ply_highres)
+    voids_b.write_to_disk(voids_lowres)    
     voids.write_to_disk(voids_highres)
+    surf = voids.export_void_mesh_with_texture("sizes")
+    surf.write_ply(ply_highres)
 
-
-    # complete: save stuff    
-    Vp = np.zeros(p_voids.vol_shape, dtype = np.uint8)
-    p_voids.fill_patches_in_volume(x_voids, Vp)
-    ds_save = DataFile('/data02/MyArchive/aisteer_3Dencoders/tmp_data/test_y_pred', tiff = True, d_shape = Vp.shape, d_type = np.uint8, VERBOSITY=0)
-    ds_save.create_new(overwrite=True)
-    ds_save.write_full(Vp)
-    Vp_mask = np.zeros(p_voids.vol_shape, dtype = np.uint8) # Save for illustration purposes the guessed neighborhood of the surface
-    p_voids.fill_patches_in_volume(np.ones((len(p_voids),wd,wd,wd)), Vp_mask)
-    ds_save = DataFile('/data02/MyArchive/aisteer_3Dencoders/tmp_data/test_y_surf', tiff = True, d_shape = Vp_mask.shape, d_type = np.uint8, VERBOSITY=0)
-    ds_save.create_new(overwrite=True)
-    ds_save.write_full(Vp_mask)
+    # # complete: save stuff    
+    # Vp = np.zeros(p_voids.vol_shape, dtype = np.uint8)
+    # p_voids.fill_patches_in_volume(x_voids, Vp)
+    # ds_save = DataFile('/data02/MyArchive/aisteer_3Dencoders/tmp_data/test_y_pred', tiff = True, d_shape = Vp.shape, d_type = np.uint8, VERBOSITY=0)
+    # ds_save.create_new(overwrite=True)
+    # ds_save.write_full(Vp)
+    # Vp_mask = np.zeros(p_voids.vol_shape, dtype = np.uint8) # Save for illustration purposes the guessed neighborhood of the surface
+    # p_voids.fill_patches_in_volume(np.ones((len(p_voids),wd,wd,wd)), Vp_mask)
+    # ds_save = DataFile('/data02/MyArchive/aisteer_3Dencoders/tmp_data/test_y_surf', tiff = True, d_shape = Vp_mask.shape, d_type = np.uint8, VERBOSITY=0)
+    # ds_save.create_new(overwrite=True)
+    # ds_save.write_full(Vp_mask)
     

tomo_encoders/reconstruction/recon.py
@@ -503,10 +503,10 @@ def recon_binning(projs, theta, center, b_K, b, apply_fbp = True, TIMEIT = False
         [_, nz, n] = projs.shape
         
         # option 1: average pooling
-        # projs = projs[::b_K].copy()
-        # data = cp.array(projs.reshape(projs.shape[0], nz//b, b, n//b, b).mean(axis=(2,4)))
+        projs = projs[::b_K].copy()
+        data = cp.array(projs.reshape(projs.shape[0], nz//b, b, n//b, b).mean(axis=(2,4)))
         # option 2: simple binning
-        data = cp.array(projs[::b_K, ::b, ::b].copy())
+        # data = cp.array(projs[::b_K, ::b, ::b].copy())
 
         # theta and center
         theta = cp.array(theta[::b_K], dtype = 'float32')

tomo_encoders/structures/voids.py
@@ -167,6 +167,10 @@ class Voids(dict):
             self["cpts"].append([int((s[i].start)) for i in range(3)])
             self["x_voids"].append(void)
 
+        self["sizes"] = np.asarray(self["sizes"])
+        self["cents"] = np.asarray(self["cents"])
+        self["cpts"] = np.asarray(self["cpts"])
+
         # copy over any other keys
         for key in voids_b.keys():
             if key in self.keys():
@@ -174,17 +178,18 @@ class Voids(dict):
             else:
                 self[key] = voids_b[key]
                 
-        
         end_chkpt.record(); end_chkpt.synchronize(); t_chkpt = cp.cuda.get_elapsed_time(st_chkpt,end_chkpt)
         print(f"\tTIME: importing voids data from grid {t_chkpt/1000.0:.2f} secs")
         return self
 
-    def guess_voids(self, projs, theta, center, b, b_K, boundary_loc = (0,0,0)):
+    def guess_voids(self, V_bin, b, boundary_loc = (0,0,0)):
 
-        # reconstruct and segment
-        V_bin = coarse_segmentation(projs, theta, center, b_K, b, 0.5)
-        Vl, n_det = label(V_bin,structure = cp.ones((3,3,3),dtype=cp.uint8))
-        Vl = Vl.get()
+        if type(V_bin) is cp._core.core.ndarray:
+            Vl, n_det = label(V_bin,structure = cp.ones((3,3,3),dtype=cp.uint8))
+            Vl = Vl.get()
+        else:
+            Vl, n_det = label(V_bin,structure = np.ones((3,3,3),dtype=np.uint8))
+            
         boundary_id = Vl[boundary_loc]
         slices = find_objects(Vl)
         print(f"\tSTAT: voids found - {n_det}")
@@ -229,6 +234,8 @@ class Voids(dict):
         selection type could be geq or leq.  
 
         '''
+        if size_thresh_um <= 0:
+            return
         size_thresh = size_thresh_um/(self.b*pixel_size_um)
         idxs = np.arange(len(self["sizes"]))
         cond_list = np.asarray([1 if self["sizes"][idx] >= size_thresh**3 else 0 for idx in idxs])
@@ -239,15 +246,27 @@ class Voids(dict):
         print(f'\tSTAT: size thres: {size_thresh:.2f} pixel length for {size_thresh_um:.2f} um threshold')          
         self.select_by_indices(idxs)
         return
-    
-    def select_around_void(self, void_id, radius):
-        idxs = np.arange(len(self["sizes"]))
+
+    def sort_by_size(self, reverse = False):
         
-        dist = np.linalg.norm((self["cents"] - self["cents"][void_id]), \
-               ord = 2, axis = 1)
+        '''
+        selection type could be geq or leq.  
+
+        '''
+        idxs = np.argsort(self["sizes"])
+        if reverse:
+            idxs = idxs[::-1]    
+        self.select_by_indices(idxs)
+        return
+
+    def select_around_void(self, void_id, radius_um, pixel_size_um = 1.0):
+        '''select voids around a given void within a spherical region'''
+        
+        radius_pix = radius_um/(self.b*pixel_size_um)
+        idxs = np.arange(len(self["sizes"]))
+        dist = np.linalg.norm((self["cents"] - self["cents"][void_id]), ord = 2, axis = 1)
         self["distance_%i"%void_id] = dist
-        cond_list = dist < radius
-        cond_list[0] = 0 # skip the sample boundary
+        cond_list = dist < radius_pix
         idxs = idxs[cond_list == 1]
         self.select_by_indices(idxs)
         return
@@ -272,7 +291,7 @@ class Voids(dict):
         return p3d_sel, r_fac
 
 
-    def _void2mesh(self, void_id, texture_key):
+    def _void2mesh(self, void_id, tex_vals):
 
         void = self["x_voids"][void_id]
         spt = self["cpts"][void_id]
@@ -292,22 +311,39 @@ class Voids(dict):
         verts *= (self.b)
 
         # set texture based on normalized size
-        texture = np.empty((len(verts),3), dtype = np.uint8)
-        
-        color = (self[texture_key][void_id] - self.min_size)/(self.max_size - self.min_size)
-        if texture_key == "sizes":
-            color = np.cbrt(color)
-
-        texture[:,0] = 255.0*color
-        texture[:,1] = 255
+        texture = np.empty((len(verts),3), dtype = np.float32)
+        texture[:,0] = tex_vals[void_id]
+        texture[:,1] = void_id
         texture[:,2] = 255
-
 
         return Surface(verts, faces, texture=texture)
 
+
+    def _gray_to_rainbow(self, gray):
+        '''
+        implements short rainbow colormap
+        https://www.particleincell.com/2014/colormap/
+
+        '''        
+        a = (1-gray)/0.25
+        X = int(a)
+        Y = int(255*(gray-X))
+        if X == 0:
+            r = 255; g = Y; b = 0
+        elif X == 1:
+            r = 255-Y; g = 255; b = 0
+        elif X == 2:
+            r = 0; g = 255; b = Y
+        elif X == 3:
+            r = 0; g = 255-Y; b = 255
+        elif X == 4:
+            r = 0; g = 0; b = 255
+        return r, g, b
     
     def export_void_mesh_with_texture(self, texture_key):
 
+        '''export with texture, slower but vis with color coding
+        '''
         st_chkpt = cp.cuda.Event(); end_chkpt = cp.cuda.Event(); st_chkpt.record()    
         
         id_len = 0
@@ -315,12 +351,20 @@ class Voids(dict):
         faces = []
         texture = []
 
-        self.mean_size = np.mean(self[texture_key])
-        self.max_size = np.max(self[texture_key])
-        self.min_size = np.min(self[texture_key])
+        
+        if texture_key == "sizes":
+            tex_vals = np.log(self["sizes"]+1.0e-12)
+            # perc = 0.95
+            # tex_vals = self[texture_key].copy()
+            # tex_thresh = tex_vals[np.argsort(tex_vals)[int(perc*len(self))]]
+            # tex_vals[tex_vals > tex_thresh] = tex_thresh
+        elif "distance" in texture_key:
+            tex_vals = self[texture_key].copy()
+        
+
 
         for iv in range(len(self)):
-            surf = self._void2mesh(iv, texture_key)
+            surf = self._void2mesh(iv, tex_vals)
             if not np.any(surf["faces"]):
                 continue
             verts.append(surf["vertices"])
@@ -328,9 +372,22 @@ class Voids(dict):
             texture.append(surf["texture"])
             id_len = id_len + len(surf["vertices"])
 
+
+        # normalize colormap
+        texture = np.concatenate(texture, axis = 0)
+        for i3 in range(3):
+            color = texture[:,i3]
+            min_val = color.min()
+            max_val = color.max()
+            if max_val > min_val:
+                texture[:,i3] = 255*(color - min_val)/(max_val - min_val)
+            else:
+                texture[:,i3] = 255
+
+
         surf = Surface(np.concatenate(verts, axis = 0), \
                        np.concatenate(faces, axis = 0), \
-                       texture = np.concatenate(texture, axis = 0))
+                       texture = texture.astype(np.uint8))
         
         end_chkpt.record(); end_chkpt.synchronize(); t_chkpt = cp.cuda.get_elapsed_time(st_chkpt,end_chkpt)
         print(f"\tTIME: compute void mesh {t_chkpt/1000.0:.2f} secs")

tomo_encoders/tasks/surface_determination.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*- 
 """ 
 """ 
+from operator import mod
+from tomo_encoders.misc.voxel_processing import modified_autocontrast
 from tomo_encoders.reconstruction.recon import recon_binning, recon_patches_3d
 import cupy as cp
 import numpy as np
@@ -17,20 +19,20 @@ def coarse_segmentation(projs, theta, center, b_K, b, blur_sigma):
     '''    
     st_rec = cp.cuda.Event(); end_rec = cp.cuda.Event(); st_rec.record()
     V_bin = recon_binning(projs, theta, center, b_K, b, blur_sigma = blur_sigma)    
+    
+    print("\tTODO: modified_autocontrast invokes GPU-CPU transfer. need to implement it on GPU")
+    min_max = modified_autocontrast(V_bin.get(), s = 0.05, normalize_sampling_factor=1)
+
     end_rec.record(); end_rec.synchronize(); t_rec = cp.cuda.get_elapsed_time(st_rec,end_rec)
     print(f"\tTIME reconstructing with binning - {t_rec/1000.0:.2f} secs")
     
     # segmentation
     thresh = cp.float32(threshold_otsu(V_bin[::4,::4,::4].reshape(-1).get()))
     V_bin = (V_bin < thresh).astype(cp.uint8)
-    return V_bin    
+    return V_bin, min_max    
 
 
-def guess_surface(projs, theta, center, b, b_K, wd):
-    ## P-GUESS ##
-    # reconstruction
-
-    V_bin = coarse_segmentation(projs, theta, center, b_K, b, 0.5).get()
+def guess_surface(V_bin, b, wd):
     
     # find patches on surface
     wdb = int(wd//b)
@@ -49,7 +51,7 @@ def guess_surface(projs, theta, center, b, b_K, wd):
     print(f"\tSTAT: r value: {eff*100.0:.2f}")        
     return p3d_surf, p3d_ones, p3d_zeros
 
-def process_patches(projs, theta, center, fe, p_surf):
+def process_patches(projs, theta, center, fe, p_surf, min_max):
 
     # SCHEME 1: integrate reconstruction and segmention (segments data on gpu itself)
     # st_proc = cp.cuda.Event(); end_proc = cp.cuda.Event(); st_proc.record()
@@ -65,10 +67,10 @@ def process_patches(projs, theta, center, fe, p_surf):
                                       apply_fbp =True)
     end_rec.record(); end_rec.synchronize(); t_rec = cp.cuda.get_elapsed_time(st_rec,end_rec)
     st_seg = cp.cuda.Event(); end_seg = cp.cuda.Event(); st_seg.record()
-    min_max = x_surf[:,::4,::4,::4].min(), x_surf[:,::4,::4,::4].max()
+    
+    x_surf = np.clip(x_surf, *min_max)
     x_surf = fe.predict_patches("segmenter", x_surf[...,np.newaxis], 256, None, min_max = min_max)[...,0]
     end_seg.record(); end_seg.synchronize(); t_seg = cp.cuda.get_elapsed_time(st_seg,end_seg)
-    t_surf = t_rec + t_seg
     
     print(f'\tTIME: local reconstruction - {t_rec/1000.0:.2f} secs')    
     print(f'\tTIME: local segmentation - {t_seg/1000.0:.2f} secs')